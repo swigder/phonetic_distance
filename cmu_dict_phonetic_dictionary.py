dc9d59fd007ca963bf86ce7e95ce0d42029db239
suri <swigder@gmail.com>
2015-10-17 16:29:52 -0400

    phonetic dictionary

--- cmu_dict_phonetic_dictionary.py
+++ cmu_dict_phonetic_dictionary.py
@@ -1,7 +1,11 @@
-import nltk
 from nltk.corpus import cmudict
 
 
-def print_pronunciation():
-    cmudict.dict()
-    print(cmudict.entries()[653:659])
+def get_pronunciation(word):
+    """
+    Gets the pronunciation from CMUDict, stripping stress information
+    :param word: word for which to look up pronunciation; capitalization-agnostic
+    :return: list of lists, where each list is one pronunciation, and each item in the list is an ARPAbet phone
+    """
+    pronunciations = cmudict.dict()[word.lower()]
+    return [[phone[0:-1] if phone.endswith(('0', '1', '2')) else phone for phone in pronunciation] for pronunciation in pronunciations]
--- test/test_cmu_dict_phonetic_dictionary.py
+++ test/test_cmu_dict_phonetic_dictionary.py
@@ -1,9 +1,12 @@
 import pytest
-from cmu_dict_phonetic_dictionary import print_pronunciation
+from cmu_dict_phonetic_dictionary import get_pronunciation
 
 
 class TestCmuDictPhoneticDictionary:
 
-    def test_print_pronunciation(self):
-        print_pronunciation()
+    def test_get_pronunciation(self):
+        assert get_pronunciation("monetary") == [['M', 'AA', 'N', 'AH', 'T', 'EH', 'R', 'IY']]
+
+    def test_get_pronunciation_upper(self):
+        assert get_pronunciation("MONETARY") == [['M', 'AA', 'N', 'AH', 'T', 'EH', 'R', 'IY']]
 
